151f3554b8fcaa6667ef8842be6e2c72aca86429
Minku Singh <mailminkusingh@gmail.com>
2022-11-29 12:18:25 +0530

    style: msg

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const currentPriceRef = document.querySelector("#current-price");
 const msg = document.querySelector("#msg");
 const tellMeRef = document.querySelector("#tell-me");
 
-function showMsg(message){
+function showMsg(message, status){
+    if(status === "fail"){
+        msg.style.color = "red";
+    }else if(status === "success"){
+        msg.style.color = "green";
+    }else{
+        msg.style.color = "gray";
+    }
     msg.style.display = "block";
     msg.innerText = message;
 }
@@ -21,14 +28,14 @@ function findProfitAndLoss(initial, current, quantity){
 
     if(initial > current){
         var loss = (initial-current)*quantity;
-        var loss_percent = (loss/initial)*100;
-        showMsg(`Oh oh, the loss is ${loss} with ${loss_percent}%`)
+        var loss_percent = ((loss/initial)*100).toFixed(2);
+        showMsg(`😪 Oh oh, the loss is ${loss} with ${loss_percent}% loss percentage`, "fail")
     }else if(current > initial){
         var profit = (current-initial)*quantity;
-        var profit_percent = (profit/initial)*100;
-        showMsg(`Wowiee, the profit is ${profit} & profit percentage is ${profit_percent}%`);
+        var profit_percent = ((profit/initial)*100).toFixed(2);
+        showMsg(`🎉 Wowiee, the profit is ${profit} & profit percentage is ${profit_percent}%`, "success");
     }else{
-        showMsg(`No pain, No gain and No gain, No pain`);
+        showMsg(`➡ No pain, No gain and No gain, No pain`);
     }
 }
 
